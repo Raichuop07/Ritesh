8452eb64e4ad617746e6fb74297b32df74d982ad
SATYAM <126665079+Raichuop07@users.noreply.github.com>
2023-05-22 22:50:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "5635825262").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Raichuop07/Raichu")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Raichuop07/Ritesh")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
@@ -76,11 +76,11 @@ clean = {}
 autoclean = []
 
 
-START_IMG_URL = getenv("START_IMG_URL", "https://telegra.ph/file/a85421dcaa998be15d475.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://telegra.ph/file/757aec5bc3023af18beda.jpg")
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
-    "https://telegra.ph/file/a85421dcaa998be15d475.jpg",
+    "https://telegra.ph/file/29746321a7161c86c5780.jpg",
 )
 
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
@@ -130,9 +130,9 @@ if UPSTREAM_REPO:
 if PING_IMG_URL:
     if PING_IMG_URL != "assets/Ping.jpeg":
         if not re.match("(?:http|https)://", PING_IMG_URL):
-            PING_IMG_URL = "https://telegra.ph/file/a85421dcaa998be15d475.jpg"
+            PING_IMG_URL = "https://telegra.ph/file/29746321a7161c86c5780.jpg"
 
 if START_IMG_URL:
     if START_IMG_URL != "assets/Ping.jpeg":
         if not re.match("(?:http|https)://", START_IMG_URL):
-            START_IMG_URL = "https://telegra.ph/file/a85421dcaa998be15d475.jpg"
+            START_IMG_URL = "https://telegra.ph/file/757aec5bc3023af18beda.jpg"
